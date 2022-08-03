0ea54655d25985273df17a5ba93a94389f94dc3e
Ihtiyar91 <ikhtiyargaliev@gmail.com>
2022-08-03 10:23:23 +0300

    Success Test

--- src/test/java/ru/netology/rest/PostmanEchoTest1.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest1.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest1 {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Пока"))
+                .body("data", equalTo("Привет"))
         ;
     }
 }
